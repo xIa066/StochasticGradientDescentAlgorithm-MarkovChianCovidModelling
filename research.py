4452d5e0dd12fe0cd8b49b35d0277d9d7adce112
Zexi Liu <511635739@qq.com>
2020-05-28 03:52:50 +1000

    -initial draft

---
+++ research.py
@@ -0,0 +1,26 @@
+# Define Cost function, lambda function, p function, alpha function
+def cost(theta:float) -> float:
+    return theta
+def la(tehta:float) -> float:
+    return 1/theta
+def p(theta:float) -> float:
+    return 1/theta
+def al(theta:float)-> float:
+    return theta
+
+
+# @ param N: is the number of total population
+# @ param T: the time that we want to simulate the process (6 month)
+def simulation_infected(la, p, al, N, T):
+    X = []
+
+
+    
+# q i _ i+1
+
+# q i _ i-1
+
+# transition probability from state i to i+1
+
+# transition probability from state i to i-1
+

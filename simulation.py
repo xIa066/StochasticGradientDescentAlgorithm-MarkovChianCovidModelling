2c5fa9bdd9e5b86fb9d26f65783ca404e0af9ab3
刘泽曦 <xiao66@Tchaikovsky.local>
2020-06-23 01:06:28 +1000

    commented code + adding update p

---
+++ simulation.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+# simulate the infected patients number for a specific period T_total
+# this function simulate not strictly numbers of X_n in T_total time, given fixed Lambda, P, Alpha
+# Random Horizon: means that the whole trajectory stop once X_n hits 0
+# @ param N: is the number of total population can be infected
+# @ param current_time: a number that is the current time when starting the T-time simulation
+# @ param x_0: initial infected patients (greater than 0)
+# @ param T_total: is total time for this simulation
+# @ param maxi_time: is the maximum time that the whole simulation process can go, can be infinity
+# @ param H: is the hospital capacity
+def simulation_infected(la, p, al, N, t_0, x_0, T_total, H, maxTime):
+    # initialize time and infected patient
+    t_0 = 0
+    # bind variables to initial conditions
+    x_n = x_0
+    # make sure the start is not 0
+    assert(x_n!=0)
+    # assign initial time to a know value, noted it can start from
+    # not just at 0 time
+    t_n = t_0
+    # introduce T_H := "the total time above the hospital capacity"
+    T_H = 0
+    # initiate X_n process trajectory and corresponding T_n process
+    X_trajectory = []
+    T_trajectory = []
+    # loop
+    # break condition
+    # @ condition1: when the time exceeds the maximum time
+    # @ condition2: when the infected patients go to zero
+    # @ condition3: when the whole populations are infected! :(
+    while (t_n<maxTime and
+           x_n != 0 and
+           x_n < N
+          ):
+        i = x_n
+        # q i _ i+1
+        q_forward_i = la*p*2*x_n*(N-i)/(N*(N-1))
+        # q i _ i-1
+        q_backward_i = al*i
+        # waiting time rate v_i = (q i _ i+1) + (q i _ i-1)
+        v_i = q_forward_i + q_backward_i
+        t_i = np.random.exponential(v_i)
+        # calculate T_H before jumping
+        if (x_n >= H):
+            T_H += t_i
+        # jumping probability to STATE i+1 is (q i _ i+1)/v_i
+        jump = np.random.binomial(n=1,p=(q_forward_i/v_i))
+        if (jump ==1):
+            x_n += 1
+        elif (jump == 0):
+            x_n -= 1            
+        # add the jumped X_n at t_i time
+        X_trajectory.append(x_n)
+        # increase time
+        T = t_n+t_i
+        T_trajectory.append(T)
+        t_n = T
+        # return when the total time of simulation is exceeded
+        if ((t_n-t_0) > T_total):
+            return X_trajectory, T_trajectory, T_H, T
+    # also return when the while break condition met
+    return X_trajectory, T_trajectory, T_H, T

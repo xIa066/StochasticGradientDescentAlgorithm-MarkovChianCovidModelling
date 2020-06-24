8a460135b59ef7ae456c82fe722dc663e16c50ff
jjsun10 <jamessun04@gmail.com>
2020-06-24 19:55:21 +1000

    Update seq_tiling.py
    target track stationary dist, convergence criteria not set yet

--- seq_tiling.py
+++ seq_tiling.py
@@ -10,33 +10,34 @@ def simProcess(l, p, a, N, T, x0, H):
     x = [x0]
 
     while i > 0 and i < N and t_n < T:
-            q_plus = l*p*2*i*(N-i)/(N*(N-1))
-            q_minus = a*i
-            v_i = q_plus + q_minus
+        q_plus = l*p*2*i*(N-i)/(N*(N-1))
+        q_minus = a*i
+        v_i = q_plus + q_minus
 
-            t_i = np.random.exponential(v_i)
-            jump = np.random.binomial(n=1, p = (q_plus/v_i))
-            t_n += t_i
+        t_i = np.random.exponential(v_i)
+        jump = np.random.binomial(n=1, p = (q_plus/v_i))
+        t_n += t_i
 
-            if t_n < T:
-                if jump == 1:
-                    i += 1
-                else:
-                    i -= 1
+        if t_n < T:
+            if jump == 1:
+                i += 1
+            else:
+                i -= 1
 
-                if i > H:
-                    t_h += t_i
+            if i > H:
+                t_h += t_i
 
-                    t.append(t_n)
-                    x.append(i)
-            
-            if t_n > T and i > H:
-                t_i += 14 - t[-1]
+            if i == 0:
+                i = 1
 
-    if t_n != 0:
-        return (i, t_h/t_n)
+            t.append(t_n)
+            x.append(i)
+        
+        if t_n > T and i > H:
+            t_h += T - t[-1]
 
-count = 0
+    return (i, t_h, t, x)
+
 l = 0.1
 a = 0
 b = 0.05
@@ -46,21 +47,39 @@ N = 4e6
 T = 14
 x0 = 10
 H = 2e4
-eps = [0.005, 2e-9]
+eps = [0.01, 2e-10]
+x = []
+t = []
+t_n = 0
+t_h = 0
+i = 0
+all_l = []
+all_a = []
+l_i = []
+a_i = []
 
-while abs(pr - b) > 0.01 and x0 > 0:
-    x0, pr = simProcess(l, p, a, N, T, x0, H)
+while i < 100 and x0 > 0:
+    x0, t_h_temp, t_temp, x_temp = simProcess(l, p, a, N, T, x0, H)
+    t += [t1 + T*i for t1 in t_temp]
+    t_n += T
+    t_h += t_h_temp
+    x += x_temp
+
+    pr = t_h/t_n
     print(pr)
 
-    if count == 0:
-        l = l - eps[0]*(pr - b)
-        if l < 0.05:
-            l = 0.05
-        count = 1
-    else:
-        a = a + eps[1]*(pr - b)
-        if a < 0:
-            a = 0
-        elif a > 1e-7:
-            a = 1e-7
-        count = 1
+    l -= eps[0]*(pr - b)
+    if l < 0.001:
+        l = 0.001
+    all_l.append(l)
+    l_i.append(len(all_l))
+
+    a += eps[1]*(pr - b)
+    if a < 0:
+        a = 0
+    elif a > 1e-7:
+        a = 1e-7
+    all_a.append(a)
+    a_i.append(len(all_a))
+
+    i += 1
